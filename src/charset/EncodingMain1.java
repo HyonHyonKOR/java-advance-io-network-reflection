1dfc228cbf215f795edebeca8e2f34331046978d
HyonHyon <khm3626@gmail.com>
2024-10-29 20:47:11 +0900

    feat:decoding

--- src/charset/EncodingMain1.java
+++ src/charset/EncodingMain1.java
@@ -1,12 +1,11 @@
 package charset;
 
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import static java.nio.charset.StandardCharsets.*;
 
-public class EncodingMain {
+public class EncodingMain1 {
 
     private static final Charset EUC_KR = Charset.forName("EUC_KR");
     private static final Charset MS_949 = Charset.forName("MS949");
@@ -25,10 +24,6 @@ public class EncodingMain {
         encoding("가", MS_949);
         encoding("가", UTF_8);
         encoding("가", UTF_16BE);
-        System.out.println("==ASCII 영문 처리==");
-
-        System.out.println("==ASCII 영문 처리==");
-
     }
 
     private static void encoding(String text, Charset charset) {
